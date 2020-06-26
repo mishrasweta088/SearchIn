e48f8e1d75d582298b0e73dd9411edb028303c5a
rausjha <raushankumarjha789@gmail.com>
2020-06-26 19:32:42 +0530

    modification in main activity and Register option

--- app/src/main/java/com/cg/project/searchin/MainActivity.kt
+++ app/src/main/java/com/cg/project/searchin/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.cg.project.searchin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    //    views
+    internal lateinit var mRegisterBtn: Button
+    internal lateinit var mLoginBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Init
+        mRegisterBtn = findViewById(R.id.register_btn)
+        mLoginBtn = findViewById(R.id.login_btn)
+
+        // handle register button click
+        mRegisterBtn.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //start Registration Option
+                val intent = Intent(this@MainActivity, RegisterOption::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
--- app/src/main/java/com/cg/project/searchin/RegisterOption.kt
+++ app/src/main/java/com/cg/project/searchin/RegisterOption.kt
@@ -1,11 +1,38 @@
 package com.cg.project.searchin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.widget.Button
 
 class RegisterOption : AppCompatActivity() {
+
+    internal lateinit var mRegisterOrganization: Button
+    internal lateinit var mRegisterUser: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_option)
+
+        mRegisterOrganization = findViewById(R.id.registerOrganisation_btn)
+        mRegisterUser = findViewById(R.id.registerUser_btn)
+
+        mRegisterOrganization.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //start Registration Option
+                val intent = Intent(this@RegisterOption, RegisterOrganisation::class.java)
+                startActivity(intent)
+            }
+        })
+
+        mRegisterUser.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                //start Registration Option
+                val intent = Intent(this@RegisterOption, RegisterUser::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
