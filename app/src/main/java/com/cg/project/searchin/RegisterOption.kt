88b4c3411ec01f9e3da72a83033b0f38db2b8850
siddharthlenka <siddharthlenka7@gmail.com>
2020-06-26 14:56:16 +0530

    Registration option layout

---
+++ app/src/main/java/com/cg/project/searchin/RegisterOption.kt
@@ -0,0 +1,11 @@
+package com.cg.project.searchin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterOption : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_option)
+    }
+}
