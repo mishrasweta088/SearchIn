b00dcc5a50cd545d9f8a0831b5fe86930251d70e
pavanibuchupalli1993 <b.pavanireddy225@gmail.com>
2020-06-29 12:17:09 +0530

    Register Issue

--- app/src/main/java/com/cg/project/searchin/UserRegisterrr.kt
+++ app/src/main/java/com/cg/project/searchin/UserRegisterrr.kt
@@ -21,6 +21,8 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import java.lang.Exception
 
@@ -36,7 +38,7 @@ class UserRegisterrr : AppCompatActivity() {
     lateinit var mProgressBar : ProgressDialog
     var isValid : Boolean = true
 
-    //Declare an instance of FirebaseAuth
+    //Firebase references
     private var mAuth: FirebaseAuth? = null
 
 
@@ -64,6 +66,7 @@ class UserRegisterrr : AppCompatActivity() {
         //In the onCreate() method, initialize the FirebaseAuth instance.
         mAuth = FirebaseAuth.getInstance();
 
+
         //handle register button click
         mRegister.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
@@ -74,70 +77,80 @@ class UserRegisterrr : AppCompatActivity() {
                 val password :String =mPassword.getText().toString().trim()
 
                 // validate
-                validate()
+                if(validate()) {
 
-                //register the user
-                registerUser(email,password)
+                    //register the user
+                    registerUser(email, password)
+                }
             }
         })
     }
 
 
-    private fun validate() {
+    private fun validate() :Boolean{
         // Check for a valid name.
+        var flag:Boolean=true
         if (mFirstName.getText().toString().isEmpty()) {
             mFirstName.setError(getResources().getString(R.string.name_error))
+            flag=false
         }
         if (mLastName.getText().toString().isEmpty()) {
             mLastName.setError(getResources().getString(R.string.name_error))
+            flag=false
         }
 
         // Check for a valid email address.
         if (mEmail.getText().toString().isEmpty()) {
             mEmail.setError(getResources().getString(R.string.email_error));
+            flag=false
         } else if (!Patterns.EMAIL_ADDRESS.matcher(mEmail.getText().toString()).matches()) {
             mEmail.setError(getResources().getString(R.string.error_invalid_email));
+            flag=false
         }
 
         // Check for a valid password.
         else if (mPassword.getText().toString().isEmpty()) {
             mPassword.setError(getResources().getString(R.string.password_error));
+            flag=false
         } else if (mPassword.getText().length < 6) {
             mPassword.setError(getResources().getString(R.string.error_invalid_password));
+            flag=false
         }
 
         //checked checkbox
-        if (mCheckbox.isChecked) {
-            mCheckbox.setChecked(true)
+        if (!mCheckbox.isChecked) {
+            Toast.makeText(this,"Check the terms and conditions",Toast.LENGTH_LONG).show()
+            flag=false
         }
+        return flag
     }
 
 
 
     private fun registerUser(email: String, password: String) {
         mProgressBar!!.show()
-        mAuth!!.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener(
-                this, OnCompleteListener <AuthResult>() {
-                    @Override
-                    fun onComplete(task: Task<AuthResult>) {
-                        if (task.isSuccessful) {
-                            //Sign in success, dismiss dialog and start register activity
-                            mProgressBar.dismiss()
-                            val user: FirebaseUser? = mAuth!!.getCurrentUser()
-                            if (user != null) {
-                                Toast.makeText(this@UserRegisterrr,"Registered...\n"+user.email, Toast.LENGTH_SHORT).show()
-                                val intent = Intent(this@UserRegisterrr, UserProfile::class.java)
-                                startActivity(intent)
-                                finish()
-                            }
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            mProgressBar.dismiss()
-                            Toast.makeText(this@UserRegisterrr,"Authentication failed.", Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }).addOnFailureListener(OnFailureListener (){
+
+      mAuth!!.createUserWithEmailAndPassword(email, password)
+          .addOnCompleteListener(this) { task ->
+              if (task.isSuccessful) {
+                  //Sign in success, dismiss dialog and start register activity
+                  mProgressBar.dismiss()
+                  val user: FirebaseUser? = mAuth!!.getCurrentUser()
+                  if (user != null) {
+                      Toast.makeText(this@UserRegisterrr,"Registered...\n"+user.email, Toast.LENGTH_SHORT).show()
+                      val intent = Intent(this@UserRegisterrr, UserProfile::class.java)
+                      startActivity(intent)
+                      finish()
+                  }
+              } else {
+                  // If sign in fails, display a message to the user.
+                  mProgressBar.dismiss()
+                  Toast.makeText(this@UserRegisterrr,"Authentication failed.", Toast.LENGTH_SHORT).show()
+              }
+
+              // ...
+          }
+          .addOnFailureListener(OnFailureListener (){
                 @Override fun onFailure(e : Exception){
                     //error, dismiss progress dialog and show the error message
                     mProgressBar.dismiss()
