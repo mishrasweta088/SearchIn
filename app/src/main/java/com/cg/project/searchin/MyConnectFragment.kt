1090c34d4409708b24c299785df6154402fe0ec8
mishrasweta088 <mishrasweta088@gmail.com>
2020-07-01 00:48:16 +0530

    MyConnectFragment.kt

--- app/src/main/java/com/cg/project/searchin/MyConnectFragment.kt
+++ app/src/main/java/com/cg/project/searchin/MyConnectFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 //import com.project.searchin.R
 
@@ -13,12 +15,22 @@ import android.view.ViewGroup
  */
 class MyConnectFragment : Fragment() {
 
+    var recycleView: RecyclerView? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_connect, container, false)
+        val view: View =inflater.inflate(R.layout.fragment_my_connect, container, false)
+
+        //init recyclerview
+        recycleView = view.findViewById(R.id.connect_recyclerView)
+        //set it's properties
+        recycleView?.setHasFixedSize(true)
+        recycleView?.setLayoutManager(LinearLayoutManager(activity))
+        return view
+
+
     }
 
 }
