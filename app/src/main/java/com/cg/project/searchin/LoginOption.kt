a41685c41cba539ccac4600dfeb1c008cb218617
siddharthlenka <siddharthlenka7@gmail.com>
2020-06-30 23:33:47 +0530

    LoginOption Updated

---
+++ app/src/main/java/com/cg/project/searchin/LoginOption.kt
@@ -0,0 +1,11 @@
+package com.cg.project.searchin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginOption : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_option)
+    }
+}
